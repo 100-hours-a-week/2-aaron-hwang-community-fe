c7a54114704443c08c7e758e63924035888c0d59
Hwang Jiwon <cutty82432@gmail.com>
2024-10-29 14:42:13 +0900

    [chore] 라이브러리 설치, DB 연결

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ import helmet from 'helmet';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import indexRouter from './routes/index.js';
-import userRouter from './routes/user.js';
+import userRouter from './routes/auth.js';
+import dotenv from 'dotenv';
+import mysql from 'mysql';
+import moment from 'moment';
+import { connect } from 'http2';
 
+dotenv.config();
+const { db_host, db_user, db_pass } = process.env;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 const publicPath = `${__dirname}/public`;
+const connection = mysql.createConnection({
+    host: db_host,
+    user: db_user,
+    password: db_pass,
+    database: 'community'
+});
+connection.connect(() => {
+    console.log('DB connect success');
+});
 
 app.use(express.static(publicPath));
 //app.use(timeout('5s'));
