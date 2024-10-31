0c7da3451fff668ce1a6a69f698314736a4913d2
Hwang Jiwon <cutty82432@gmail.com>
2024-11-01 00:19:39 +0900

    feat: 게시글 수정 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ const limiter = ratelimit({
 router.get('/', timeout('3s'), limiter, index.getIndex);
 router.get('/posts', index.getPosts);
 router.get('/posts/create', index.getPostCreate);
+router.get('/posts/edit/:id', index.getPostEdit);
 router.get('/posts/:id', index.getPostDetail);
 
 export default router;
