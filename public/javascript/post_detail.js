15d2df3a1927653e31ed172b584cc5b1b4890272
Hwang Jiwon <cutty82432@gmail.com>
2024-11-03 22:14:48 +0900

    fix: 게시글 상세조회 기능, css 수정

--- public/javascript/post_detail.js
+++ public/javascript/post_detail.js
@@ -10,8 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const modalMessage = document.getElementById("modalMessage");
     const modalConfirm = document.getElementById("modalConfirm");
     const modalCancel = document.getElementById("modalCancel");
+    const likes = document.getElementById("likes-wrapper");
+    const views = document.getElementById("views-wrapper");
+    const comments = document.getElementById("comments-wrapper");
     const postId = window.location.pathname.split('/').pop();
     
+    const dummyPost = {
+        id: 1,
+        title: "첫 번째 게시글입니다. 이 내용은 테스트입니다.",
+        author: {
+            username: 'Aaron',
+            profile_img: '/images/profile_img.jpg'
+        },
+        createdAt: '2024-11-01 10:00:00',
+        likes: 12000,
+        comments: 350,
+        views: 20000
+    }
+
     // 뒤로 가기 버튼
     backButton.addEventListener("click", () => {
         history.back();
@@ -19,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // 게시글 수정 버튼
     editPost.addEventListener("click", () => {
-        window.location.href = `/post/edit/${postId}`;
+        window.location.href = `/posts/edit/${postId}`;
     });
     
 
@@ -31,6 +47,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    likes.innerHTML = `${dummyPost.likes >= 1000 ? (dummyPost.likes / 1000).toFixed(1) + 'k' : dummyPost.likes}<br>좋아요수`;
+    views.innerHTML = `${dummyPost.views >= 1000 ? (dummyPost.views / 1000).toFixed(1) + 'k' : dummyPost.views}<br>좋아요수`;
+    comments.innerHTML = `${dummyPost.comments >= 1000 ? (dummyPost.comments / 1000).toFixed(1) + 'k' : dummyPost.comments}<br>좋아요수`;
     // 댓글 입력 시 등록 버튼 활성화/비활성화
     commentInput.addEventListener("input", () => {
         if (commentInput.value.trim() !== "") {
