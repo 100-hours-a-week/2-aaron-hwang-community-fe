b133b3cfc51136f9a20e01b4be9a9c956d990a4e
Hwang Jiwon <cutty82432@gmail.com>
2024-11-07 16:13:40 +0900

    feat: fetch json 형식으로 게시글 목록 조회 구현

--- public/javascript/posts.js
+++ public/javascript/posts.js
@@ -9,94 +9,58 @@ document.addEventListener('DOMContentLoaded', () => {
     const passwordEdit = document.querySelector(".password-edit");
     const logout = document.querySelector(".logout");
 
-    // 더미 데이터 생성
-    const dummyPosts = [
-        {
-            id: 1,
-            title: "첫 번째 게시글입니다. 이 내용은 테스트입니다.",
-            author: {
-                username: 'Aaron',
-                profile_img: '/images/profile_img.jpg'
-            },
-            createdAt: '2024-11-01 10:00:00',
-            likes: 12000,
-            comments: 350,
-            views: 200000
-        },
-        {
-            id: 2,
-            title: "제목 2",
-            author: {
-                username: "Edwin",
-                profile_img: '/images/profile_img.jpg'
-            },
-            createdAt: "2024-11-02 09:30:00",
-            likes: 800,
-            comments: 150,
-            views: 950
-        },
-        {
-            id: 3,
-            title: "Hi, I'm Leo. title over 26 characters",
-            author: {
-                username: "Leo",
-                profile_img: '/images/profile_img.jpg'
-            },
-            createdAt: "2024-11-02 09:30:00",
-            likes: 8000,
-            comments: 150,
-            views: 2412412
-        },
-        {
-            id: 4,
-            title: "Stella, Team Leader of 9rm",
-            author: {
-                username: "Stella",
-                profile_img: '/images/profile_img.jpg'
-            },
-            createdAt: "2024-11-02 09:30:00",
-            likes: 1000,
-            comments: 0,
-            views: 1000
-        },
-    ];
-
-    // 더미 데이터를 HTML에 렌더링
-    console.log('Dummy rendering')
     const postsContainer = document.getElementById('post-list');
-    dummyPosts.forEach(post => {
-        const postElement = document.createElement('fieldset');
-        postElement.className = `post-outerline`;
+    fetch("/data/posts.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        data.forEach(post => {
+            console.log(post)
+            
 
-        // 게시글 제목, 날짜, 작성자 등 표시
-        postElement.innerHTML = `
-            <div class="post-item">
-                <div class="post-header">
-                    ${post.title.length > 26 ? post.title.slice(0, 26) + "..." : post.title}
-                </div>
-                <div class="post-body">
-                    <div class="post-header-wrapper">
-                        <span class="post-likes">좋아요 ${post.likes >= 1000 ? (post.likes / 1000).toFixed(1) + 'k' : post.likes}</span>
-                        <span class="post-views">댓글 ${post.comments >= 1000 ? (post.likes / 1000).toFixed(1) + 'k' : post.comments}</span>
-                        <span class="post-reply">조회수 ${post.views >= 1000 ? (post.views / 1000).toFixed(1) + 'k' : post.views}</span>    
+            fetch("/data/users.json")
+            .then((response) => response.json())
+            .then((data) => {
+                const author = data.users.find((author) => author.id === post.author_id)
+                
+                const postElement = document.createElement('fieldset');
+                postElement.className = `post-outerline`;
+
+                // 게시글 제목, 날짜, 작성자 등 표시
+                postElement.innerHTML = `
+                    <div class="post-item">
+                        <div class="post-header">
+                            ${post.title.length > 26 ? post.title.slice(0, 26) + "..." : post.title}
+                        </div>
+                        <div class="post-body">
+                            <div class="post-header-wrapper">
+                                <span class="post-likes">좋아요 ${post.likes >= 1000 ? (post.likes / 1000).toFixed(1) + 'k' : post.likes}</span>
+                                <span class="post-views">댓글 ${post.comments >= 1000 ? (post.likes / 1000).toFixed(1) + 'k' : post.comments}</span>
+                                <span class="post-reply">조회수 ${post.views >= 1000 ? (post.views / 1000).toFixed(1) + 'k' : post.views}</span>    
+                            </div>
+                            <div class="post-header-wrapper">
+                                <span class="post-date">${post.createdAt}</span>
+                            </div>
+                        </div>                       
                     </div>
-                    <div class="post-header-wrapper">
-                        <span class="post-date">${post.createdAt}</span>
+                    <div class="post-footer">
+                        <img src="${author.profile_img}"></img>
+                        <span class="post-author">${author.username}</span>
+
                     </div>
-                </div>                       
-            </div>
-            <div class="post-footer">
-                <img src="${post.author.profile_img}"></img>
-                <span class="post-author">${post.author.username}</span>
+                `;
+                console.log(postElement.innerHTML);
+                postElement.addEventListener('click', () => {
+                    window.location.href = `/posts/${post.id}`;
+                });
 
-            </div>
-        `;
-        postElement.addEventListener('click', () => {
-            window.location.href = `/posts/${post.id}`;
-        });
+                postsContainer.appendChild(postElement);
+                })
 
-        postsContainer.appendChild(postElement);
-    });
+            })
+
+            
+    })
     // 클릭 이벤트 리스너 추가
     userEdit.addEventListener("click", () => {
         // 회원정보 수정 페이지로 이동하는 예제 코드
