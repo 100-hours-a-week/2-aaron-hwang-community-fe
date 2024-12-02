8f68b0df768d0ef6cfe98d8c0ab3a014a788c607
JiwonHwang <cutty82432@gmail.com>
2024-12-03 00:07:14 +0900

    fix: 로그아웃 fetch 수정

--- public/javascript/post_create.js
+++ public/javascript/post_create.js
@@ -90,27 +90,25 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "/auth/change-password/1";
     });
 
-    logout.addEventListener("click", () => {
-        logout.addEventListener("click", async () => {
-            try {
-                const response = await fetch('http://localhost:8000/api/auth/logout', {
-                    method: 'POST',
-                    // credentials: 'include', // 세션 쿠키를 포함
-                });
-    
-                if (response.ok) {
-                    const result = await response.json();
-                    alert(result.message || '로그아웃 성공!');
-                    window.location.href = '/'; // 로그아웃 후 로그인 페이지로 이동
-                } else {
-                    const error = await response.json();
-                    alert(error.message || '로그아웃 실패!');
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('로그아웃 요청 중 오류가 발생했습니다.');
+    logout.addEventListener("click", async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/auth/logout', {
+                method: 'POST',
+                // credentials: 'include', // 세션 쿠키를 포함
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                alert(result.message || '로그아웃 성공!');
+                window.location.href = '/'; // 로그아웃 후 로그인 페이지로 이동
+            } else {
+                const error = await response.json();
+                alert(error.message || '로그아웃 실패!');
             }
-            
-        });
+        } catch (error) {
+            console.error('Error:', error);
+            alert('로그아웃 요청 중 오류가 발생했습니다.');
+        }
+        
     });
 });
